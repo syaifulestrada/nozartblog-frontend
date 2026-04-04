6eca82a505e6c45cd201b6ed2ad5d6071d8d1c74
syaifulestrada <syaifultheminister@gmail.com>
2026-04-04 10:39:12 +0700

    install tailwindcss and shadcnui

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <button
-                className="px-2 py-2 bg-green-500 text-white"
-                onClick={() => {
-                    setCount((count) => count + 1);
-                }}
-            >
-                {count}
-            </button>
-        </>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
+        </div>
     );
 }
 
